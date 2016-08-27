aaf9ec0841a5689434215fcb7e1d8dc595977bde
denispeplin <denis.peplin@gmail.com>
2016-08-27 06:11:40 +0300

    Use Plug.Router instead of homemade plug.

--- lib/plug_app.ex
+++ lib/plug_app.ex
@@ -1,22 +1,11 @@
 defmodule PlugApp do
   use Application
-  import Plug.Conn
 
   def start(_type, _args) do
-    children = [Plug.Adapters.Cowboy.child_spec(:http, PlugApp, [])]
+    children = [Plug.Adapters.Cowboy.child_spec(:http, PlugApp.Router, [])]
 
     opts = [strategy: :one_for_one, name: PlugApp.Supervisor]
 
     Supervisor.start_link(children, opts)
-  end
-
-  def init(options) do
-    options
-  end
-
-  def call(conn, _opts) do
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello world")
   end
 end
